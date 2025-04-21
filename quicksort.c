bf7e21e468aaf83df1645b432fb655a4cc0eafa6
wallet-77 <roryivor@gmail.com>
2025-04-21 11:43:44 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,53 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void fnSwap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+void swapElements(int *firstElement, int *secondElement) {
+    int temporary = *firstElement;
+    *firstElement = *secondElement;
+    *secondElement = temporary;
 }
 
-int fnPartition(int a[], int l, int r) {
-    int p = a[l];
-    int i = l;
-    int j = r + 1;
+int partitionArray(int array[], int lowIndex, int highIndex) {
+    int pivotValue = array[lowIndex];
+    int i = lowIndex;
+    int j = highIndex + 1;
     do {
-        do { i++; } while (i <= r && a[i] < p);
-        do { j--; } while (a[j] > p);
-        fnSwap(&a[i], &a[j]);
+        do { i++; } while (i <= highIndex && array[i] < pivotValue);
+        do { j--; } while (array[j] > pivotValue);
+        swapElements(&array[i], &array[j]);
     } while (i < j);
-    fnSwap(&a[i], &a[j]);
-    fnSwap(&a[l], &a[j]);
+    swapElements(&array[i], &array[j]);
+    swapElements(&array[lowIndex], &array[j]);
     return j;
 }
 
-void fnQuickSort(int a[], int l, int r) {
-    int s;
-    if (l < r) {
-        s = fnPartition(a, l, r);
-        fnQuickSort(a, l, s - 1);
-        fnQuickSort(a, s + 1, r);
+void quickSortArray(int array[], int lowIndex, int highIndex) {
+    int splitPoint;
+    if (lowIndex < highIndex) {
+        splitPoint = partitionArray(array, lowIndex, highIndex);
+        quickSortArray(array, lowIndex, splitPoint - 1);
+        quickSortArray(array, splitPoint + 1, highIndex);
     }
 }
 
-void fnDispArray(int X[], int n) {
+void displayArray(int dataArray[], int numberOfElements) {
     int i;
-    for (i = 0; i < n; i++)
-        printf(" %5d ", X[i]);
+    for (i = 0; i < numberOfElements; i++)
+        printf(" %5d ", dataArray[i]);
     printf("\n");
 }
 
 int main() {
-    int iaArr[] = {5, 2, 8, 1, 9, 4, 7, 3, 6};
-    int iNum = sizeof(iaArr) / sizeof(iaArr[0]);
+    int integerArray[] = {5, 2, 8, 1, 9, 4, 7, 3, 6};
+    int elementCount = sizeof(integerArray) / sizeof(integerArray[0]);
 
     printf("\nUnsorted Array\n");
-    fnDispArray(iaArr, iNum);
+    displayArray(integerArray, elementCount);
 
-    fnQuickSort(iaArr, 0, iNum - 1);
+    quickSortArray(integerArray, 0, elementCount - 1);
 
     printf("\nSorted Array\n");
-    fnDispArray(iaArr, iNum);
+    displayArray(integerArray, elementCount);
 
     return 0;
 }
